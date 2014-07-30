ec52061f4a71b2d108e08d8999e70c4a5c2989e0
cambeezy <cameron.bolourchi@gmail.com>
2014-07-30 16:07:28 -0700

    Update kill_threads.sql

--- kill_threads.sql
+++ kill_threads.sql
@@ -1,3 +1,7 @@
-#Compile sleeping threads into a file with a kill command for each mysql thread and load the file to kill threads.
-select concat('KILL ',id,';') from information_schema.processlist where Command='Sleep' into outfile '/tmp/process.txt';
-source /tmp/process.txt;
+#Compile sleeping threads into a file with a kill command for each mysql thread and load the file to kill threads then delete the file.
+
+system rm /tmp/process.sql;
+select concat('KILL ',id,';') from information_schema.processlist where Command='Sleep' into outfile '/tmp/process.sql';
+
+source /tmp/process.sql;
+system rm /tmp/process.sql;
